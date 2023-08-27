74dee93cb79e77de78ace909e069bcd09d28d6ba
Anderson Leite <gtlt1999@gmail.com>
2023-08-27 10:02:06 -0300

    atuaizacao mobile

--- components/navbar.js
+++ components/navbar.js
@@ -13,10 +13,10 @@ const Navbar = () => {
     ];
     const links = [
       "https://systemtec.vercel.app/",
-       "https://animesonlineup.co/naruto-shippuden/",
+       "https://systemtec.vercel.app/",
        "https://systemtec.vercel.app/sistemadeautomacao/",
-       "https://facebook.com",
-       "https://instagram.com",
+       "https://systemtec.vercel.app/",
+       "https://systemtec.vercel.app/",
      ];
 
   return (
@@ -85,7 +85,7 @@ const Navbar = () => {
         {/* menu  */}
         <div className="hidden text-center lg:flex lg:items-center">
           <ul className="items-center justify-end flex-1 pt-6 list-none lg:pt-0 lg:flex">
-            {navigation.map((menu, index) => (
+            {navigation.map((menu, index, links) => (
               <li className="mr-3 nav__item" key={index}>
                 <Link href={links[index]} className="inline-block px-4 py-2 text-lg font-normal text-gray-800 no-underline rounded-md dark:text-gray-200 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 focus:outline-none dark:focus:bg-gray-800">
                     {menu}
--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,6 @@ import Navbar from "../components/navbar";
 import SectionTitle from "../components/sectionTitle";
 
 import { benefitOne, benefitTwo } from "../components/data";
-import Video from "../components/video";
 import VideoManutencao from "../components/videoManutencao";
 import Benefits from "../components/benefits";
 import Footer from "../components/footer";
