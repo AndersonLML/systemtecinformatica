bbea5fe257159cfd67cb535bff90d5d786281880
Anderson Leite <68247603+AndersonLML@users.noreply.github.com>
2023-10-07 15:15:09 -0300

    update final

---
+++ pages/Tutoriais%20-%20Sistema%20de%20Automação%20Comercial/index.js
@@ -0,0 +1,109 @@
+import Head from "next/head";
+import Hero from "../../components/hero";
+import Navbar from "../../components/navbar";
+import SectionTitle from "../../components/sectionTitle";
+
+import { benefitOne, benefitTwo } from "../../components/data";
+import VideoEmissaoNFE from "../../components/videoEmissaoNFE";
+import VideoCadastroClientes from "../../components/videoCadastroClientes";
+import VideoCadastroProdutos from "../../components/videoCadastroProdutos";
+import VideoPerfilFiscal from "../../components/videoPerfilFiscal";
+import VideoPromissoria from "../../components/videoPromissoria";
+import Benefits from "../../components/benefits";
+import Footer from "../../components/footer";
+import Testimonials from "../../components/testimonials";
+import Cta from "../../components/cta";
+import Faq from "../../components/faq";
+import PopupWidget from "../../components/popupWidget";
+import VideoManutencaoProdutos from "../../components/videoManutencaoProdutos";
+import VideoSuprimentoCaixa from "../../components/videoSuprimentoCaixa";
+import VideoAberturaCaixa from "../../components/videoAberturaCaixa";
+import VideoFechamentoCaixa from "../../components/videoFechamentoCaixa";
+import VideoAtualizarProdutoBalanca from "../../components/videoAtualizarProdutoBalanca";
+
+const Home = () => {
+  return (
+    <>
+      <Head>
+        <title>System Tec Informática - Assistência Técnica em Informática, Venda de Sistemas de Automação e Criação de Sites</title>
+        <meta
+          name="description"
+          content="System Tec Informática - Assistência Técnica em Informática, Venda de Sistemas de Automação e Criação de Sites"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <Navbar />
+
+       <SectionTitle
+       pretitle="Como aumentar o Controle de Caixa Diário"
+       title="Como Fazer a Abertura de Caixa">
+      </SectionTitle>
+      <VideoAberturaCaixa />
+
+      <SectionTitle
+       title="Como Fazer um Suprimento de Caixa">
+      </SectionTitle>
+      <VideoSuprimentoCaixa />
+
+      <SectionTitle
+       title="Como Fazer o Fechamento de Caixa">
+      </SectionTitle>
+      <VideoFechamentoCaixa />
+
+      <SectionTitle
+       pretitle="Como melhorar o Controle dos Produtos"
+       title="Como Cadastrar o Produto de Forma Completa para poder Emitir NFE e Cupom Fiscal">
+      </SectionTitle>
+      <VideoCadastroProdutos />
+
+      <SectionTitle
+       title="Como alterar o preço dos Produtos pela Manutenção dos Produtos">
+      </SectionTitle>
+      <VideoManutencaoProdutos />
+
+      <SectionTitle
+       title="Como atualizar os Produtos da Balança direto pelo Sistema">
+      </SectionTitle>
+      <VideoAtualizarProdutoBalanca />
+
+      <SectionTitle
+      pretitle="Como aumentar o Controle dos Clientes"
+       title="Como Cadastrar o Cliente no Sistema de Forma Simples e Prática">
+      </SectionTitle>
+      <VideoCadastroClientes />
+      
+      <SectionTitle
+       pretitle="Configurações Fiscais e outras Funcionalidades"
+       title="Como configurar o Perfil Fiscal no Sistema com o CFOP de Venda">
+      </SectionTitle>
+      <VideoPerfilFiscal />
+
+      <SectionTitle
+        title="Como Fazer a emissão de uma NFe a partir de uma venda">
+      </SectionTitle>
+      <VideoEmissaoNFE />
+
+      <SectionTitle
+       title="Como fazer uma venda a prazo gerando a promissória da venda">
+      </SectionTitle>
+      <VideoPromissoria />
+
+      <SectionTitle
+        pretitle="Testemunhos"
+        title="Aqui estão alguns dos nossos clientes">
+        Testemunho de Clientes que utilizam nosso serviço e aprovam
+      </SectionTitle>
+      <Testimonials />
+      <SectionTitle pretitle="FAQ" title="Algumas perguntas frequentes">
+        Aqui estão algumas perguntas frequentes dos clientes
+      </SectionTitle>
+      <Faq />
+      <Cta />
+      <Footer />
+      <PopupWidget />
+    </>
+  );
+}
+
+export default Home;
