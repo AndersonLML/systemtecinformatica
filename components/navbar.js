583fc13121217cfc629b6248ba38e576e9b2c128
Anderson Leite <68247603+AndersonLML@users.noreply.github.com>
2023-10-07 15:17:18 -0300

    mais um update

--- components/navbar.js
+++ components/navbar.js
@@ -67,7 +67,7 @@ const Navbar = () => {
                   <>
                     {navigation.map((item, index) => (
                       <Link key={index} href={links[index]} className="w-full px-4 py-2 -ml-4 text-gray-500 rounded-md dark:text-gray-300 hover:text-indigo-500 focus:text-indigo-500 focus:bg-indigo-100 dark:focus:bg-gray-800 focus:outline-none">
-                          {item})
+                          {item}
                       </Link>
                     ))}
                     <Link href="https://wa.me/message/BSA4YTAIUDRUL1" className="w-full px-6 py-2 mt-3 text-center text-white bg-indigo-600 rounded-md lg:ml-5">         
