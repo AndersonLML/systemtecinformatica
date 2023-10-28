bc813c3d3713f57d0033f95036c2130b7ca0cf7a
Anderson Leite <68247603+AndersonLML@users.noreply.github.com>
2023-10-28 11:33:45 -0300

    analitycs update1.1

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import { ThemeProvider } from "next-themes";
 import "../css/tailwind.css";
+import { Analytics } from '@vercel/analytics/react';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class">
       <Component {...pageProps} />
+      <Analytics />
     </ThemeProvider>
   );
 }
