b51351131617cd4e790708b123b07eb739f9ff4c
Anderson Leite <gtlt1999@gmail.com>
2024-01-18 13:23:44 -0300

    update SpeedInsights

--- components/container.js
+++ components/container.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Analytics } from '@vercel/analytics/react';
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 
 const Container = (props) => {
@@ -10,6 +11,7 @@ const Container = (props) => {
       }`}>
       {props.children}
       <Analytics />
+      <SpeedInsights />
     </div>
   );
 }
